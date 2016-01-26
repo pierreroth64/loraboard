77ad9375b783088529b505f59c90ea7a41f82cb5
Pierre ROTH <pierre.roth@legrand.fr>
2016-01-26 15:11:01 +0100

    Change pubnub settings from popup window

--- app/js/views/settings_view.js
+++ app/js/views/settings_view.js
@@ -1,13 +1,19 @@
 
+import {SettingsStorageAPI} from '../lib/storage';
 export class SettingsView extends Backbone.View {
 
   constructor(options) {
     super(options);
+    this.events = {
+      "click #save-settings" : "saveSettings"
+    };
     this.setElement('#settings-popup');
+    this.api = new SettingsStorageAPI('pubnub');
     this.render();
   }
 
   render() {
+    var creds = this.api.get();
     var html = `<div class="modal fade" id="pubnub-setttings" tabindex="-1" role="dialog" aria-labelledby="pubnub-title">
                    <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -17,23 +23,31 @@ export class SettingsView extends Backbone.View {
                       </div>
                       <div class="modal-body">
                         <div class="input-group">
-                          <span class="input-group-addon" id="pubnub-channel">Channel</span>
-                          <input type="text" class="form-control" placeholder="PubNub channel" aria-describedby="basic-addon1">
+                          <span class="input-group-addon">Channel</span>
+                          <input type="text" id="pubnub-channel" class="form-control" placeholder="PubNub channel" aria-describedby="basic-addon1">
                         </div>
                         <br />
                         <div class="input-group">
-                          <span class="input-group-addon" id="pubnub-subscribe-key">Subscribe Key</span>
-                          <input type="text" class="form-control" placeholder="PubNub subscribe key" aria-describedby="basic-addon1">
+                          <span class="input-group-addon">Subscribe Key</span>
+                          <input type="text" id="pubnub-subscribe-key" class="form-control" placeholder="PubNub subscribe key" aria-describedby="basic-addon1">
                         </div>
                       </div>
                       <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button id="save-settings" type="button" class="btn btn-primary">Save changes</button>
                       </div>
                     </div>
                   </div>
                 </div>`;
     this.$el.html(html);
     return this;
+  }
+
+  saveSettings() {
+    var channel = $('#pubnub-channel').val();
+    var key = $('#pubnub-subscribe-key').val();
+    this.api.store({channel: channel, subscribeKey: key});
+    Backbone.Mediator.publish('settings:new');
+    $('#pubnub-setttings').modal('hide');
   }
 }
