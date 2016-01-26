5e9511d1cb4a6874e258ab0f58a9b19d7ecf9512
Pierre ROTH <pierre.roth@legrand.fr>
2016-01-26 15:10:27 +0100

    Refactor data collector to accept credentials

--- app/js/data/loramote.js
+++ app/js/data/loramote.js
@@ -1,65 +1,102 @@
 import {LoRaMoteDataDecoder} from './decoders/loramote_decoder';
+import {SettingsStorageAPI} from '../lib/storage';
 
-var PUBNUB_CHANNEL = 'pubnub pierreroth';
-var PUBNUB_SUBSCRIBE_KEY = 'sub-c-addd8e9e-b938-11e5-85eb-02ee2ddab7fe';
+var PUBNUB_DEFAULT_CHANNEL = 'pubnub pierreroth';
+var PUBNUB_DEFAULT_SUBSCRIBE_KEY = 'sub-c-addd8e9e-b938-11e5-85eb-02ee2ddab7fe';
 
 export class LoRaMoteDataCollector  {
 
     constructor(models) {
         this.name = "LoRaMote data collector through PubNub service";
         this.models = models;
-        this.pubnubConn = PUBNUB({
-                    subscribe_key: PUBNUB_SUBSCRIBE_KEY
-        });
+        this.resetConnection();
         this.decoder = new LoRaMoteDataDecoder();
         this.isStarted = undefined;
+        Backbone.Mediator.subscribe('settings:new', this.onNewSettings, this);
+    }
+
+    updateCredentials() {
+        var api = new SettingsStorageAPI('pubnub');
+        if (api.hasStoredValue('subscribeKey')) {
+            this.currentChannel = api.get('channel');
+            this.currentSubscribeKey = api.get('subscribeKey');
+        } else {
+            console.log('Pubnub credentials not set, using default ones.');
+            this.currentChannel = PUBNUB_DEFAULT_CHANNEL;
+            this.currentSubscribeKey = PUBNUB_DEFAULT_SUBSCRIBE_KEY;
+        }
+    }
+
+    createConnection() {
+        this.pubnubConn = PUBNUB({
+            subscribe_key: this.currentSubscribeKey
+        });
+    }
+
+    resetConnection() {
+        this.stop();
+        this.updateCredentials();
+        this.createConnection();
+    }
+
+    onNewSettings() {
+        this.resetConnection();
+        this.start();
     }
 
     start() {
         console.log("Starting LoRa data gathering...");
-        this.pubnubConn.subscribe({
-                channel: PUBNUB_CHANNEL,
-                message: function(message, env, ch, timer, magicCh) {
-                    if (message.data) {
-                        this.models.temp.set({value: this.decoder.decodeTemperature(message.data).value});
-                        this.models.press.set({value: this.decoder.decodePressure(message.data).value});
-                        this.models.batt.set({value: this.decoder.decodeBatteryLevel(message.data).value});
-                        this.models.position.set({value: JSON.stringify({
-                                                        latitude: this.decoder.decodeLatitude(message.data).value,
-                                                        longitude: this.decoder.decodeLongitude(message.data).value})
-                                                });
-                        var rawFrame = JSON.stringify(message);
-                        var decodedFrame = JSON.stringify(this.decoder.decode(message.data));
-                        Backbone.Mediator.publish('data:newFrame', rawFrame, decodedFrame);
-                    }
-                },
-                connect: this.onConnected,
-                disconnect: this.onDisconnected,
-                error: this.onError
-                });
-        this.isStarted = true;
+        console.log(`Pubnub info > subscribeKey: '${this.currentSubscribeKey}', and channel:'${this.currentChannel}'`);
+        try {
+            this.pubnubConn.subscribe({
+                    channel: this.currentChannel,
+                    message: function(message, env, ch, timer, magicCh) {
+                        if (message.data) {
+                            this.models.temp.set({value: this.decoder.decodeTemperature(message.data).value});
+                            this.models.press.set({value: this.decoder.decodePressure(message.data).value});
+                            this.models.batt.set({value: this.decoder.decodeBatteryLevel(message.data).value});
+                            this.models.position.set({value: JSON.stringify({
+                                                            latitude: this.decoder.decodeLatitude(message.data).value,
+                                                            longitude: this.decoder.decodeLongitude(message.data).value})
+                                                     });
+                            var rawFrame = JSON.stringify(message);
+                            var decodedFrame = JSON.stringify(this.decoder.decode(message.data));
+                            Backbone.Mediator.publish('data:newFrame', rawFrame, decodedFrame);
+                        }
+                    },
+                    connect: this.onConnected,
+                    disconnect: this.onDisconnected,
+                    error: this.onError
+                    });
+            this.isStarted = true;
+        } catch (e) {
+            var errorMsg = JSON.stringify(e);
+            console.log(`Error when subscribing to PubNub: ${errorMsg}`);
+        }
     }
 
     stop() {
-        console.log("Stopping LoRa data gathering...");
-        this.pubnubConn.unsubscribe({
-                channel: PUBNUB_CHANNEL
-        });
-        this.isStarted = false;
-        console.log("Stopped LoRa data gathering.");
+        if (this.isStarted) {
+            console.log("Stopping LoRa data gathering...");
+            this.pubnubConn.unsubscribe({
+                channel: this.currentChannel
+            });
+            this.isStarted = false;
+            console.log("Stopped LoRa data gathering.");
+        }
     }
 
     onConnected() {
-        console.log(`Connected to '${PUBNUB_CHANNEL}' channel`);
+        console.log("Connected to PubNub");
         console.log("Started LoRa data gathering.");
     }
 
     onDisconnected() {
-        console.log(`Disconnected from'${PUBNUB_CHANNEL}' channel`);
+        console.log("Disconnected from PubNub");
     }
 
     onError(error) {
         var errorMsg = JSON.stringify(error);
-        console.log(`Network error on '${PUBNUB_CHANNEL}' channel: ${errorMsg}`);
+        console.log(`Network error on PubNub: ${errorMsg}`);
     }
 }
