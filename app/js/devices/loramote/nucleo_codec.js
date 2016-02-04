b9c1d336b0b1220daee4c1634d9a3bcdb0f4864e
Pierre ROTH <pierre.roth@legrand.fr>
2016-02-04 08:33:23 +0100

    Fix nucleo codec (has to inherit from base codec)

--- app/js/devices/loramote/nucleo_codec.js
+++ app/js/devices/loramote/nucleo_codec.js
@@ -1,8 +1,17 @@
-export class NucleoCodec {
+import {BaseCodec} from '../base_codec';
 
-    constructor() { }
+export class NucleoCodec extends BaseCodec {
+
+    constructor() {
+        super();
+    }
 
     decode(frame) {
         throw new Error('NucleoCodec:decode not implented');
+    }
+
+    mayMatch(data) {
+        //FIXME: find creteria on payload to make this codec match/unmatch
+        return false;
     }
 }
