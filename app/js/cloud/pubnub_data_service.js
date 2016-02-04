596cd608bd160f042b72e1b53f7b8c5a87593187
Pierre ROTH <pierre.roth@legrand.fr>
2016-02-04 12:02:28 +0100

    Move pubnub defaults to constants

--- app/js/cloud/pubnub_data_service.js
+++ app/js/cloud/pubnub_data_service.js
@@ -1,8 +1,6 @@
 import {SettingsStorageAPI} from '../lib/storage';
 import {DataService} from './data_service';
-
-var PUBNUB_DEFAULT_CHANNEL = 'pubnub pierreroth';
-var PUBNUB_DEFAULT_SUBSCRIBE_KEY = 'sub-c-addd8e9e-b938-11e5-85eb-02ee2ddab7fe';
+import {PUBNUB_DEFAULT_SUBSCRIBE_KEY, PUBNUB_DEFAULT_CHANNEL} from '../constants/pubnub_const';
 
 export class PubNubDataService extends DataService  {
 
---
+++ app/js/constants/pubnub_const.js
@@ -0,0 +1,2 @@
+export const PUBNUB_DEFAULT_CHANNEL = 'pubnub pierreroth';
+export const PUBNUB_DEFAULT_SUBSCRIBE_KEY = 'sub-c-addd8e9e-b938-11e5-85eb-02ee2ddab7fe';
