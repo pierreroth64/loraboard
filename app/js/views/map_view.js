2d23c552606f46dc4026d3163832ae9f860f217a
Fabien RIBES <fribes@gmail.com>
2016-02-05 16:04:15 +0100

    Not working custom icon

--- app/js/views/map_view.js
+++ app/js/views/map_view.js
@@ -12,10 +12,41 @@ export class MapView extends Backbone.View {
   initMap() {
       L.mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
       var initialPosition = [43.3188648, -0.3203877];
-      this.gpsMarker = L.marker(initialPosition);
-      this.gpsMarker.bindPopup(this.buildMarkerInfo(...initialPosition));
+//      this.gpsMarker = L.marker(initialPosition);
+//      this.gpsMarker.bindPopup(this.buildMarkerInfo(...initialPosition));
       this.map = L.mapbox.map('lora-map', 'mapbox.streets').setView(initialPosition, 15);
-      this.gpsMarker.addTo(this.map);
+//      this.gpsMarker.addTo(this.map);
+
+      var myLayer = L.mapbox.featureLayer().addTo(this.map);
+
+      var geoJson = [{
+          "type": "Feature",
+          "geometry": {
+              "type": "Point",
+              "coordinates": [43.3188648, -0.3203877]
+          },
+          "properties": {
+              "title": "Mote",
+              "icon": {
+                  "iconUrl": "/images/logo_lora2.png",
+                  "iconSize": [50, 38], // size of the icon
+                  "iconAnchor": [25, 19], // point of the icon which will correspond to marker's location
+                  "popupAnchor": [0, -25], // point from which the popup should open relative to the iconAnchor
+                  "className": "dot"
+              }
+          }
+      }];
+
+      // Set a custom icon on each marker based on feature properties.
+      myLayer.on('layeradd', function(e) {
+          var marker = e.layer,
+              feature = marker.feature;
+
+          marker.setIcon(L.icon(feature.properties.icon));
+      });
+
+      // Add features to the map.
+      myLayer.setGeoJSON(geoJson);
   }
 
   updatePosition() {
