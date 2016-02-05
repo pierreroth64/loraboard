925f560cb4ff487bfe7a72bcc70035a080a61901
Pierre ROTH <pierre.roth@legrand.fr>
2016-02-05 11:08:56 +0100

    Marker is only display when GPS data is received in map view widget

--- app/js/views/map_view.js
+++ app/js/views/map_view.js
@@ -2,7 +2,8 @@ import {MAPBOX_ACCESS_TOKEN} from '../constants/mapbox_const';
 
 
 const MAP_INITIAL_POSITION = [43.3188648, -0.3203877];
-const MAP_INITIAL_ZOOM = 15;
+const MAP_INITIAL_ZOOM = 1;
+const MAP_WHEN_FIRST_FOUND_ZOOM = 15;
 
 export class MapView extends Backbone.View {
 
@@ -14,16 +15,18 @@ export class MapView extends Backbone.View {
 
   initMap() {
       L.mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
-      this.gpsMarker = L.marker(MAP_INITIAL_POSITION);
-      this.gpsMarker.bindPopup(this.buildMarkerInfo(...MAP_INITIAL_POSITION));
       this.map = L.mapbox.map(this.id, 'mapbox.streets').setView(MAP_INITIAL_POSITION, MAP_INITIAL_ZOOM);
-      this.gpsMarker.addTo(this.map);
   }
 
   updatePosition() {
     var latitude = this.model.attributes.value.latitude;
     var longitude = this.model.attributes.value.longitude;
     var latlng = L.latLng(latitude, longitude);
+    if (this.gpsMarker == undefined) {
+      this.gpsMarker = L.marker([latitude, longitude]);
+      this.gpsMarker.addTo(this.map);
+      this.map.setZoom(MAP_WHEN_FIRST_FOUND_ZOOM);
+    }
     this.gpsMarker.setLatLng(latlng);
     this.gpsMarker.closePopup();
     this.gpsMarker.bindPopup(this.buildMarkerInfo(latitude, longitude));
