0e0050e182cd5e4ad48d858049f0d6ed8ac4e527
Pierre ROTH <pierre.roth@legrand.fr>
2016-02-08 09:51:43 +0100

    Add a home link to error page

--- app/js/views/error_view.js
+++ app/js/views/error_view.js
@@ -12,8 +12,9 @@ export class ErrorView extends Backbone.View {
         </div>
       </div>
       <div class="row">
-        <h3>Page not found</h3>
+        <h3>Page not found :(</h3>
         <h5>${this.errorMsg}</h5>
+        <a href="/">Back home</a>
       </div>`;
     this.$el.html(html);
     return this;
