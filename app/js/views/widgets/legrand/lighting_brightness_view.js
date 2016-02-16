99d5d91f8bb878d2344c8a797f81fb6c9ad4b20e
Pierre ROTH <pierre.roth@legrand.fr>
2016-02-16 13:41:38 +0100

    Build brightness threshold for lighting device

--- app/js/views/widgets/legrand/lighting_brightness_view.js
+++ app/js/views/widgets/legrand/lighting_brightness_view.js
@@ -2,16 +2,30 @@ import {BrightnessGraphView} from '../brightness_view';
 
 const BRIGTHNESS_DARK_COLOR = '#000000';
 const BRIGTHNESS_BRIGHT_COLOR = '#d67b19';
-const BRIGTHNESS_LIMIT = 210;
+const DEFAULT_BRIGTHNESS_LIMIT = 210;
 
 export class LightingBrightnessGraphView extends BrightnessGraphView {
 
   constructor(options) {
     super(options);
+    this.brightnessThreshold = this.buildBrightnessThreshold();
+  }
+
+  buildBrightnessThreshold() {
+    var threshold = localStorage.getItem('brightnessThreshold');
+    try {
+      threshold = parseInt(threshold, 10);
+      if (isNaN(threshold)) {
+        throw new Error('threshold is not a number');
+      }
+      return threshold;
+    } catch (e) {
+      return DEFAULT_BRIGTHNESS_LIMIT;
+    }
   }
 
   buildColorFromBrightness(brightness) {
-    return (brightness < BRIGTHNESS_LIMIT) ? BRIGTHNESS_DARK_COLOR: BRIGTHNESS_BRIGHT_COLOR;
+    return (brightness < this.brightnessThreshold) ? BRIGTHNESS_DARK_COLOR: BRIGTHNESS_BRIGHT_COLOR;
   }
 
   initChart() {
