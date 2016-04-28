a0f3c6f161b964c1a73fcf36df2e3edb1816bca4
Pierre ROTH <pierre.roth@legrand.fr>
2016-04-28 10:07:41 +0200

    Update Pubnub credentials
    We reached the limit with previous account

--- app/js/constants/pubnub_const.js
+++ app/js/constants/pubnub_const.js
@@ -1,4 +1,4 @@
 export const PUBNUB_DEFAULT_DOWNSTREAM_CHANNEL = 'downstream';
 export const PUBNUB_DEFAULT_UPSTREAM_CHANNEL = 'upstream';
-export const PUBNUB_DEFAULT_SUBSCRIBE_KEY = 'sub-c-9866ce9a-ca86-11e5-a9aa-02ee2ddab7fe';
-export const PUBNUB_DEFAULT_PUBLISH_KEY = 'pub-c-eae40235-4d3a-48b1-8ac3-325dac057f26';
+export const PUBNUB_DEFAULT_SUBSCRIBE_KEY = 'sub-c-addd8e9e-b938-11e5-85eb-02ee2ddab7fe';
+export const PUBNUB_DEFAULT_PUBLISH_KEY = 'pub-c-c2122241-5b54-4ea7-a8c8-6bdc31aa380a';
